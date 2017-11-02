81c35c59542f2a0a5d0c3c9c61eee6af897a8c8d
John Mark Gabriel Caguicla <caguicla.jmg@gmail.com>
2017-11-02 09:55:46 +0800

    Manifest update

--- app/src/main/java/com/caguiclajmg/pibot/SplashActivity.java
+++ app/src/main/java/com/caguiclajmg/pibot/SplashActivity.java
@@ -1,13 +1,16 @@
 package com.caguiclajmg.pibot;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) actionBar.hide();
     }
 }
